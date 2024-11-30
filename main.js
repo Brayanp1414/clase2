bc3b67b7acac5fef9ac8619dec6a7adf5b34cb18
unknown <brayanrojas30253175@gmail.com>
2024-11-30 18:02:10 -0400

    operadores logicos

---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// suma
+
+
+
+
+
+
+
+
+// // division
+// alert(Num1)
+
+
+let Num3 = 2;
+Num3++;
+// alert(Num3);
+
+// operadores de comparacion
+alert(2 > 1);
+alert (2 < 1);
+alert()
